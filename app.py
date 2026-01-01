5cc3b3b3ad7bc36b89078009bbf3fad88b53952c
EthanMShort <emshort05@gmail.com>
2026-01-01 01:55:22 -0600

    Fix CPU and RAM status monitor

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import subprocess
-from flask import Flask, render_template, request, redirect, url_for
+import psutil
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__, template_folder='web/templates')
 
